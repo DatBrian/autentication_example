fbd59f50226403d9f5d4fbc920e3fd0094755131
DatBrian <vikard.2005@hotmail.com>
2023-07-01 00:29:43 -0500

    feat: :tada: Se agrego el proyecto al repositorio
    Se agregó el proyecto con un sistema de consultas y login para autenticacion con un servidor inexistente que puede funcionar como plantilla

---
+++ src/services/AuthServices.ts
@@ -0,0 +1,58 @@
+import { ref, Ref } from 'vue'
+class AuthService {
+
+    private jwt: Ref<string>;
+    private error: Ref<string>;
+
+    constructor() {
+        this.jwt = ref('');
+        this.error = ref('');
+    }
+
+    getJwt():Ref<string> {
+        return this.jwt;
+    }
+
+    getError():Ref<string> {
+        return this.error;
+    }
+
+    private config(data:any) {
+        const config = {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        }
+
+        return config;
+    }
+
+    async login(email:string, password:string):Promise<boolean> {
+        try {
+            const data = {
+                email: email,
+                password: password
+            }
+            const req = await fetch('https//url.de.prueba', this.config(data));
+
+            const res = await req.json();
+
+            if ('errors' in res) {
+                this.error.value = "Login failed";
+                return false;
+            }
+
+            this.jwt.value = res.data.access_token;
+            return true;
+
+        } catch (error) {
+            this.error.value = "Login failed";
+            return false;
+        }
+    }
+}
+
+export default AuthService;
---
+++ src/views/AuthView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="container">
+        <form>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                    v-model="email">
+                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" v-model="password">
+            </div>
+            <button type="submit" class="btn btn-primary" @click.prevent="authUser">Submit</button>
+        </form>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { Ref, ref } from 'vue'
+import AuthService from '../services/AuthServices'
+
+let email: Ref = ref("");
+let password: Ref = ref("");
+
+const authUser = async () => {
+    const auth = new AuthService();
+    const success = await auth.login(email.value, password.value);
+
+    (success) ? alert('Exito!') : alert('Login failed');
+
+}
+
+
+</script>
+
+<style scoped lang="scss"></style>
